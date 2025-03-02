c4e3ffcfd623fe7d5c95d5f6b3435994e35c9eef
Juusoz <saesvuo.juuso@gmail.com>
2025-03-02 04:39:11 +0200

    Adding speed optimizations to spurs

--- SpurGear.js
+++ SpurGear.js
@@ -62,6 +62,7 @@ self.onmessage = function (msg) {
 			//Replace the previous best deviation.
 			gear_ratio_best_deviation = Math.abs(gear_ratio_deviation);
 			
+		}
 		//Deviation bigger than the previous best deviation, no progress in this direction. || Optimization 3.
 		if(Math.abs(gear_ratio_deviation) > gear_ratio_best_deviation){			
 			noProgressHere = true;
